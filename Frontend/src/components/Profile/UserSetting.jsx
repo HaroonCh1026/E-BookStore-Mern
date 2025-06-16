598ee1e3f192c568a68f605dd8d7356ee5289056
HaroonRiaz1026 <150551308+HaroonCh1026@users.noreply.github.com>
2025-06-16 15:25:15 +0500

    Update UserSetting.jsx

--- Frontend/src/components/Profile/UserSetting.jsx
+++ Frontend/src/components/Profile/UserSetting.jsx
@@ -15,7 +15,7 @@ const UserSetting = () => {
     const fetchUserData = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/get-user-information",
+          "https://e-bookstore-mern.onrender.com/api/v1/get-user-information",
           { headers }
         );
         setProfileData(response.data.user);
